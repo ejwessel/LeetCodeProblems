89d37bb83077375c8b086f9a36c12dc5fe98db57
Ethan Wessel <ejwessel@gmail.com>
2020-01-31 00:14:37 -0500

    problem 21

---
+++ 21_MergeTwoSortedLists/README.md
@@ -0,0 +1,16 @@
+# MergeTwoSortedLists
+
+Both solutions are
+- Runtime: O(n)
+- Space: O(1)
+
+## Solution 1
+- It has 3 logic blocks
+- First block handles merging two lists together
+- The last two blocks handle what happens when one of the lists is empty
+
+## Solution 2
+- It has 3 logic blocks
+- First block handles merging two lists together and handles what happens when one of the lists is empty
+- This solution is interesting since it's faster than the first solution, but arguably more confusing to understand
+- This goes to show that you can optimize like crazy, but code becomes much harder to understand. 
---
+++ 21_MergeTwoSortedLists/Solution_2.py
@@ -0,0 +1,107 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def mergeTwoLists(self, l1, l2):
+        head = None
+        tail = head
+        # handle logic when combining the lists
+        while (l1 is not None) or (l2 is not None):
+            # handle if a list if empty
+            if l1 is None or l2 is None:
+                # handle when there are left overs in a list
+                if (l1 is not None) and (l2 is None):
+                    if head is None:
+                        head = l1
+                    else:
+                        tail.next = l1
+                    l1 = None
+                if (l2 is not None) and (l1 is None):
+                    if head is None:
+                        head = l2
+                    else:
+                        tail.next = l2
+                    l2 = None
+            else:
+                # choose l1 over l2
+                if l1.val <= l2.val:
+                    if head is None:
+                        head = l1
+                        tail = head
+                    else:
+                        tail.next = l1
+                        tail = tail.next
+                    # move head pointer of l1 to next node
+                    l1 = l1.next
+                # Choose l2 over l1
+                elif l2.val < l1.val:
+                    if head is None:
+                        head = l2
+                        tail = head
+                    else:
+                        tail.next = l2
+                        tail = tail.next
+                    # move head pointer of l1 to next node
+                    l2 = l2.next
+        return head
+
+def print_list(list_to_print):
+    current = list_to_print
+    while current is not None:
+        print(current.val)
+        current = current.next
+
+def ll_from_list(list_to_convert):
+    head = None
+    current = head
+    for item in list_to_convert:
+        newNode = ListNode(item)
+        if head is None:
+            head = newNode
+            current = head
+        else:
+            current.next = newNode
+            current = current.next
+    return head
+
+
+if __name__ == "__main__":
+    list1 = ll_from_list([1, 2, 4])
+    list2 = ll_from_list([1, 3, 4])
+    result = Solution().mergeTwoLists(list1, list2)
+    print_list(result)
+    print()
+
+    list1 = ll_from_list([])
+    list2 = ll_from_list([1, 3, 4])
+    result = Solution().mergeTwoLists(list1, list2)
+    print_list(result)
+    print()
+
+    list2 = ll_from_list([])
+    list1 = ll_from_list([1, 3, 4])
+    result = Solution().mergeTwoLists(list1, list2)
+    print_list(result)
+    print()
+
+    list1 = ll_from_list([9, 13, 15])
+    list2 = ll_from_list([1, 3, 4])
+    result = Solution().mergeTwoLists(list1, list2)
+    print_list(result)
+    print()
+
+    list1 = ll_from_list([9, 9, 9])
+    list2 = ll_from_list([9, 9, 9])
+    result = Solution().mergeTwoLists(list1, list2)
+    print_list(result)
+    print()
+
+    list1 = ll_from_list([])
+    list2 = ll_from_list([])
+    result = Solution().mergeTwoLists(list1, list2)
+    print_list(result)
+    print()
