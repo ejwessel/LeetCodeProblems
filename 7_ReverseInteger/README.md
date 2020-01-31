b6e8bc1cbb2d027ca93fe450bbdbee807f450a47
Ethan Wessel <ejwessel@gmail.com>
2020-01-31 00:06:45 -0500

    problem 7

---
+++ 7_ReverseInteger/README.md
@@ -0,0 +1,14 @@
+# ReverseInteger
+
+## Solution 1:
+- Runtime: O(n)
+  - needs to look through the entire number in string form
+  
+- Space: O(n)
+  - intermittently saves the string
+
+## Solution 2:
+- Runtime: O(n)
+  - needs to look up to half the string to reverse it
+- Space: O(n)
+  - itermittently saves the new string
---
+++ 7_ReverseInteger/Solution_2.py
@@ -0,0 +1,54 @@
+MAX_INT = (1 << 31) - 1
+MIN_INT = -(1 << 31)
+
+
+class Solution(object):
+
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        sign = False
+        if x < 0:
+            sign = True
+
+        x_str = str(abs(x))
+        new_str = x_str[::-1]
+
+        # convert string back into int
+        new_int = int(new_str)
+        if sign:
+            new_int *= -1
+
+        # check boundary cases
+        if new_int > MAX_INT or new_int < MIN_INT:
+            return 0
+
+        return new_int
+
+
+if __name__ == "__main__":
+    sol = Solution().reverse(23)
+    assert sol == 32
+
+    sol = Solution().reverse(230)
+    assert sol == 32
+
+    sol = Solution().reverse(-32)
+    assert sol == -23
+
+    sol = Solution().reverse(123)
+    assert sol == 321
+
+    sol = Solution().reverse(-123)
+    assert sol == -321
+
+    sol = Solution().reverse(120)
+    assert sol == 21
+
+    sol = Solution().reverse(1 << 32)
+    assert sol == 0
+
+    sol = Solution().reverse(-(1 << 32))
+    assert sol == 0
