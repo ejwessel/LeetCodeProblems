07ae7a0ec01b6f114b77a272bb3154cc3935d074
Ethan Wessel <ethan.wessel@fluidity.io>
2020-01-31 17:18:32 -0500

    problem 55 checkpoint inoptimal method

--- 55_JumpGame/Solution.py
+++ 55_JumpGame/Solution.py
@@ -2,60 +2,59 @@ from typing import List
 
 
 class Solution:
-    def canJump(self, nums: List[int]) -> bool:
-        can_access_end = [False] * len(nums)
+    def canJump_inoptimal(self, nums: List[int]) -> bool:
+        can_access_end = list(bytearray(len(nums)))
 
         for i in reversed(range(len(nums))):
             # for when we start at the end
             if i == len(nums) - 1:
-                can_access_end[i] = True
+                can_access_end[i] = 1
             else:
-                can_access = False
-
-                # limit how many elements we need to look over
-                limit = min(i + nums[i] + 1, len(nums))
-
-                # start 1 after current index
-                for k in range(i + 1, limit):
-                    if can_access_end[k]:
-                        can_access_end[i] = True
-                        break
+                sub_list = can_access_end[i: min(i + nums[i] + 1, len(nums))]
+                output = int(''.join(map(str, sub_list)), 2)
+                # alternative way
+                #output = sum(c << i for i, c in enumerate(sub_list))
+                if output > 0:
+                    can_access_end[i] = 1
 
         # answer will be at the front
         return can_access_end[0]
+
+    def canJump(self, nums: List[int]) -> bool:
+        pass
 
 
 if __name__ == "__main__":
     sol = Solution()
 
     nums = [1]
-    result = sol.canJump(nums)
+    result = sol.canJump_inoptimal(nums)
     assert result
 
     nums = [2, 3, 1, 1, 4]
-    result = sol.canJump(nums)
+    result = sol.canJump_inoptimal(nums)
     assert result
 
     nums = [3, 2, 1, 0, 4]
-    result = sol.canJump(nums)
+    result = sol.canJump_inoptimal(nums)
     assert not result
 
     nums = [9, 9, 9, 9, 1]
-    result = sol.canJump(nums)
+    result = sol.canJump_inoptimal(nums)
     assert result
 
     nums = [9, 0, 0, 0, 1]
-    result = sol.canJump(nums)
+    result = sol.canJump_inoptimal(nums)
     assert result
 
     nums = [0, 9, 9, 9, 1]
-    result = sol.canJump(nums)
+    result = sol.canJump_inoptimal(nums)
     assert not result
 
     nums = [1, 2, 0, 1]
-    result = sol.canJump(nums)
+    result = sol.canJump_inoptimal(nums)
     assert result
 
     nums = [1, 1, 0, 1]
-    result = sol.canJump(nums)
+    result = sol.canJump_inoptimal(nums)
     assert not result
