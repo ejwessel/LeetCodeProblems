c4b24b6fb84493195dea8304b9dec803457ed507
Ethan Wessel <ethan.wessel@fluidity.io>
2020-02-02 16:16:32 -0500

    fix

--- 60_PermutationSequence/Solution.py
+++ 60_PermutationSequence/Solution.py
@@ -3,6 +3,12 @@ from itertools import permutations
 
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
+        '''
+        obtains the kth target permutation
+        :param n: n! elements that we need to identify k over
+        :param k: the number of the element in the permutation list
+        :return: the permutation at the kth element
+        '''
 
         list = []
         for i in range(n):
@@ -12,16 +18,23 @@ class Solution:
         result = ''.join(result)
         return result
 
-    def recursiveFind(self, list, k, lower_rng):
+    def recursiveFind(self, list, k, prev_lower_rng):
+        '''
+        recursively identifies the target k permutation element
+        :param list: the list of remaining items
+        :param k: the target k desired
+        :param prev_lower_rng: the lower range of how many permutations are below us
+        :return: the list of items to form the kth permutation
+        '''
         if len(list) == 1:
             return [str(list[0])]
 
         # find the range closest to our target that doesn't go over
         selected_idx = 0
-        lower_range = selected_idx
+        lower_range = prev_lower_rng
         for j in range(len(list)):
             # -1 for remaining items in list that will be used
-            upper_range = lower_rng + (j + 1) * factorial(len(list) - 1)
+            upper_range = prev_lower_rng + (j + 1) * factorial(len(list) - 1)
             selected_idx = j
             if upper_range >= k:
                 break
@@ -47,6 +60,9 @@ if __name__ == "__main__":
     #     if count == 360000:
     #         print(i)
     #     count += 1
+
+    result = sol.getPermutation(4, 7)
+    assert result == '2134'
 
     result = sol.getPermutation(1, 1)
     assert result == '1'
