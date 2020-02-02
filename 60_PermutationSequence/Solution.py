417bc5e69d6abfc48162ba676e69490063e11e4e
Ethan Wessel <ethan.wessel@fluidity.io>
2020-02-02 15:52:58 -0500

    Problem 60 Solution

---
+++ 60_PermutationSequence/Solution.py
@@ -0,0 +1,58 @@
+from math import factorial
+
+class Solution:
+    def getPermutation(self, n: int, k: int) -> str:
+
+        if n == 0:
+            return "1"
+
+        list = []
+        for i in range(n):
+            list.append(i + 1)
+
+        result = self.recursiveFind(list, k, 0)
+        result = ''.join(result)
+        return result
+
+    def recursiveFind(self, list, k, lower_rng):
+        if len(list) == 1:
+            return [str(list[0])]
+
+        # find the range closest to our target that doesn't go over
+        selected_idx = 0
+        lower_range = selected_idx
+        for j in range(len(list)):
+            # -1 for remaining items in list that will be used
+            upper_range = lower_rng + (j + 1) * factorial(len(list) - 1)
+            selected_idx = j
+            if upper_range >= k:
+                break
+            lower_range = upper_range
+
+        list_without_selected = list[:selected_idx] + list[selected_idx + 1:]
+        result = [str(list[selected_idx])] + self.recursiveFind(list_without_selected, k, lower_range)
+        return result
+
+
+
+if __name__ == "__main__":
+    sol = Solution()
+
+    # what about when n = 0, k = 0, n = 1, k = 1, other edge cases
+    result = sol.getPermutation(0, 1)
+    assert result == '1'
+
+    result = sol.getPermutation(1, 1)
+    assert result == '1'
+
+    result = sol.getPermutation(4, 9)
+    assert result == '2314'
+
+    result = sol.getPermutation(3, 3)
+    assert result == '213'
+
+    result = sol.getPermutation(4, 22)
+    assert result == '4231'
+
+    result = sol.getPermutation(4, 1)
+    assert result == '1234'
