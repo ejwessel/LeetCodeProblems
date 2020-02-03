78f07a04469abda5595870899812a1dc15189371
Ethan Wessel <ethan.wessel@fluidity.io>
2020-02-03 16:01:40 -0500

    final solution for problem 63

--- 63_UniquePaths2/Solution.py
+++ 63_UniquePaths2/Solution.py
@@ -2,6 +2,28 @@ from typing import List
 
 
 class Solution:
+    def setupMatrix(self, obstacleGrid):
+        m = len(obstacleGrid[0])
+        n = len(obstacleGrid)
+
+        matrix = []
+        for row in range(n):
+            matrix.append([0] * m)
+
+        # set up columns
+        for col in range(len(matrix[0])):
+            if obstacleGrid[0][col] == 1:
+                break
+            matrix[0][col] = 1
+
+        # set up rows
+        for row in range(len(matrix)):
+            if obstacleGrid[row][0] == 1:
+                break
+            matrix[row][0] = 1
+
+        return matrix
+
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
 
         m = len(obstacleGrid[0])
@@ -9,34 +31,10 @@ class Solution:
 
         if m <= 0 or n <= 0:
             return 0
-        elif m == 1 or n == 1:
-            sum = 0
-            for row in range(len(obstacleGrid)):
-                for col in range(len(obstacleGrid[0])):
-                    sum += obstacleGrid[row][col]
+        elif m == 1 and n == 1 and obstacleGrid[0][0] != 1:
+            return 1
 
-            if sum > 0:
-                return 0
-            else:
-                return 1
-
-        matrix = []
-        for row in range(n):
-            matrix.append([0] * m)
-
-        # set up columns
-        for col in range(1, len(matrix[0])):
-            if obstacleGrid[0][col] == 1:
-                matrix[0][col] = 0
-                continue
-            matrix[0][col] = 1
-
-        # set up rows
-        for row in range(1, len(matrix)):
-            if obstacleGrid[row][0] == 1:
-                matrix[row][0] = 0
-                continue
-            matrix[row][0] = 1
+        matrix = self.setupMatrix(obstacleGrid)
 
         # compute over matrix
         for row in range(1, len(matrix)):
@@ -53,6 +51,16 @@ class Solution:
 
 if __name__ == "__main__":
     sol = Solution()
+
+    obstacle_matrix = \
+        [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [1, 1, 1, 1],
+            [0, 0, 0, 0]
+        ]
+    result = sol.uniquePathsWithObstacles(obstacle_matrix)
+    assert result == 0
 
     obstacle_matrix = \
         [
@@ -127,13 +135,6 @@ if __name__ == "__main__":
 
     obstacle_matrix = \
         [
-            [0],
-        ]
-    result = sol.uniquePathsWithObstacles(obstacle_matrix)
-    assert result == 1
-
-    obstacle_matrix = \
-        [
             [0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0],
             [0, 0, 1, 1, 0],
@@ -141,3 +142,36 @@ if __name__ == "__main__":
         ]
     result = sol.uniquePathsWithObstacles(obstacle_matrix)
     assert result == 5
+
+    obstacle_matrix = \
+        [
+            [0],
+        ]
+    result = sol.uniquePathsWithObstacles(obstacle_matrix)
+    assert result == 1
+
+    obstacle_matrix = \
+        [
+            [1],
+        ]
+    result = sol.uniquePathsWithObstacles(obstacle_matrix)
+    assert result == 0
+
+    obstacle_matrix = \
+        [
+            [0, 0, 0, 1, 0],
+            [0, 0, 0, 1, 0],
+            [0, 0, 1, 1, 0],
+            [1, 1, 1, 0, 0],
+            [0, 0, 0, 0, 0],
+        ]
+    result = sol.uniquePathsWithObstacles(obstacle_matrix)
+    assert result == 0
+
+    obstacle_matrix = \
+        [
+            [1, 0],
+        ]
+    result = sol.uniquePathsWithObstacles(obstacle_matrix)
+    assert result == 0
+
