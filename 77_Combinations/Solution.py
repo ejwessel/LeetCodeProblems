28b232ea78cf904d1f99f4e0769c648c3496bba7
Ethan Wessel <ejwessel@gmail.com>
2020-02-04 23:54:07 -0500

    checkpoint

--- 77_Combinations/Solution.py
+++ 77_Combinations/Solution.py
@@ -1,11 +1,28 @@
 from typing import List
 
+
 class Solution:
-    def combine(self, n: int, k: int) -> List[List[int]]:
-        pass
+    def __init__(self):
+        self.solutions = []
+
+    def combine(self, n: int, k: int):
+        self._combine(n, k, 1, [])
+        return self.solutions
+
+    def _combine(self, n: int, k: int, start: int, sol: List) -> List[List[int]]:
+        if len(sol) == k:
+            self.solutions.append(sol)
+        else:
+            bound = n - (k - (start))
+            for i in range(start, bound):
+                self._combine(n, k, i + 1, sol + [i])
+
 
 if __name__ == "__main__":
     sol = Solution()
+
+    result = sol.combine(4, 4)
+    print(result)
 
     n = 4
     k = 2
@@ -29,4 +46,3 @@ if __name__ == "__main__":
             for c in range(b + 1, n):
                 for d in range(c + 1, n + 1):
                     print(a, b, c, d)
-
