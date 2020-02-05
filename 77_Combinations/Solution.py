60bb40418a50a9d93e6c9764453e71188b4d3c92
Ethan Wessel <ethan.wessel@fluidity.io>
2020-02-04 19:52:54 -0500

    testing outputs

--- 77_Combinations/Solution.py
+++ 77_Combinations/Solution.py
@@ -7,5 +7,26 @@ class Solution:
 if __name__ == "__main__":
     sol = Solution()
 
-    result = sol.combine(4, 2)
-    assert result == None
+    n = 4
+    k = 2
+    for a in range(1, n):
+        for b in range(a + 1, n + 1):
+            print(a, b)
+    print()
+
+    n = 4
+    k = 3
+    for a in range(1, n + 1):
+        for b in range(a + 1, n + 1):
+            for c in range(b + 1, n + 1):
+                print(a, b, c)
+    print()
+
+    n = 4
+    k = 4
+    for a in range(1, n - 2):
+        for b in range(a + 1, n - 1):
+            for c in range(b + 1, n):
+                for d in range(c + 1, n + 1):
+                    print(a, b, c, d)
+
