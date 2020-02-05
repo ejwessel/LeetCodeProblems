85613f4ce35f6098849f59768c0875bbd55c9072
Ethan Wessel <ethan.wessel@fluidity.io>
2020-02-05 10:07:20 -0500

    optimized iteration

--- 77_Combinations/Solution.py
+++ 77_Combinations/Solution.py
@@ -6,6 +6,8 @@ class Solution:
         self.solutions = []
 
     def combine(self, n: int, k: int):
+        # +1 to n because we start counting at 1 and so the upper range needs to be 1 higher
+        # 1 to start because we start counting at 1
         self._combine(n + 1, k, 1, [])
         return self.solutions
 
@@ -13,42 +15,46 @@ class Solution:
         if len(sol) == k:
             self.solutions.append(sol)
         else:
-            for i in range(start, n):
+            # the bound is computed to optimize the upper bound
+            bound = min(n, n - (k - start))
+            # in of bound and n so that we don't go over n
+            for i in range(start, bound):
                 self._combine(n, k, i + 1, sol + [i])
 
 
 if __name__ == "__main__":
+    # sol = Solution()
+    # result = sol.combine(4, 0)
+    # assert result == [[]]  # may need to be []
+    #
+    # sol = Solution()
+    # result = sol.combine(4, 1)
+    # assert result == [[1], [2], [3], [4]]
+
     sol = Solution()
     result = sol.combine(4, 2)
-    print(result)
+    assert result == [[1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4]]
 
     sol = Solution()
     result = sol.combine(4, 3)
-    print(result)
+    assert result == [[1, 2, 3], [1, 2, 4], [1, 3, 4], [2, 3, 4]]
 
     sol = Solution()
     result = sol.combine(4, 4)
-    print(result)
+    assert result == [[1, 2, 3, 4]]
 
-    n = 4
-    k = 2
-    for a in range(1, n):
-        for b in range(a + 1, n + 1):
-            print(a, b)
-    print()
+    sol = Solution()
+    result = sol.combine(4, 5)
+    assert result == []
 
-    n = 4
-    k = 3
-    for a in range(1, n + 1):
-        for b in range(a + 1, n + 1):
-            for c in range(b + 1, n + 1):
-                print(a, b, c)
-    print()
+    sol = Solution()
+    result = sol.combine(0, 1)
+    assert result == []
 
-    n = 4
-    k = 4
-    for a in range(1, n - 2):
-        for b in range(a + 1, n - 1):
-            for c in range(b + 1, n):
-                for d in range(c + 1, n + 1):
-                    print(a, b, c, d)
+    sol = Solution()
+    result = sol.combine(1, 1)
+    assert result == [[1]]
+
+    sol = Solution()
+    result = sol.combine(2, 1)
+    assert result == [[1], [2]]
