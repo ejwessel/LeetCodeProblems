b5fa1825870864219df6889d758fddd02c343745
Ethan Wessel <ethan.wessel@fluidity.io>
2020-02-04 19:22:14 -0500

    Problem 77 Setup

---
+++ 77_Combinations/Solution.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        pass
+
+if __name__ == "__main__":
+    sol = Solution()
+
+    result = sol.combine(4, 2)
+    assert result == None
