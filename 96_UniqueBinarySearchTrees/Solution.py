b14d8203876c5aa81c5c07762f0ab8d3f05efc17
Ethan Wessel <ethan.wessel@fluidity.io>
2020-02-12 13:48:59 -0500

    cut out fluff

--- 96_UniqueBinarySearchTrees/Solution.py
+++ 96_UniqueBinarySearchTrees/Solution.py
@@ -1,38 +1,6 @@
-from typing import List
-
-# Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
 
 class Solution:
-    def generateTrees(self, n: int) -> List[TreeNode]:
-        if n == 0:
-            return []
-        # [1, n + 1] because we go from 1 to n inclusive
-        return self.generateTreesHelper(1, n + 1)
-
-    def generateTreesHelper(self, start, end):
-        if start >= end:
-            return [None]
-
-        subtrees = []
-        for i in range(start, end):
-            left_subtrees = self.generateTreesHelper(start, i)
-            right_subtrees = self.generateTreesHelper(i + 1, end)
-
-            # construct this level tree before passing up
-            for left in left_subtrees:
-                for right in right_subtrees:
-                    new_node = TreeNode(i)
-                    new_node.left = left
-                    new_node.right = right
-                    subtrees.append(new_node)
-        return subtrees
-
-    def numTrees(self, n: int) -> List[TreeNode]:
+    def numTrees(self, n: int) -> int:
         if n == 0:
             return 0
         # [1, n + 1] because we go from 1 to n inclusive
@@ -55,43 +23,19 @@ class Solution:
                 tree_count += right_subtree_count
         return tree_count
 
-def printPreOrderTraversal(node: TreeNode):
-    if node is None:
-        return [None]
-    output = [node.val]
-    output += printPreOrderTraversal(node.left)
-    output += printPreOrderTraversal(node.right)
-    return output
-
-def printResults(trees):
-    for tree in trees:
-        print(printPreOrderTraversal(tree))
-
 if __name__ == "__main__":
     sol = Solution()
-    results = sol.generateTrees(0)
-    print(len(results))
+    result = sol.numTrees(0)
+    assert result == 0
 
     sol = Solution()
-    results = sol.generateTrees(3)
-    print(len(results))
+    result = sol.numTrees(3)
+    assert result == 5
 
     sol = Solution()
-    results = sol.numTrees(3)
-    print(results)
+    result = sol.numTrees(4)
+    assert result == 14
 
     sol = Solution()
-    results = sol.generateTrees(4)
-    print(len(results))
-
-    sol = Solution()
-    results = sol.numTrees(4)
-    print(results)
-
-    sol = Solution()
-    results = sol.generateTrees(5)
-    print(len(results))
-
-    sol = Solution()
-    results = sol.numTrees(5)
-    print(results)
+    result = sol.numTrees(5)
+    assert result == 42
