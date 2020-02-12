66cff74750bd6aea6a688f2b14ee58b9874866a2
Ethan Wessel <ethan.wessel@fluidity.io>
2020-02-12 17:43:26 -0500

    Problem 102 Setup

---
+++ 102_BinaryTreeLevelOrderTraversal/Solution.py
@@ -0,0 +1,16 @@
+from typing import List
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        pass
+
+if __name__ == "__main__":
+    sol = Solution()
+    sol.levelOrder()
