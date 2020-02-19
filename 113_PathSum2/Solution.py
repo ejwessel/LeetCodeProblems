c51d363600d1d3e4ffe50382fadc62a471329268
Ethan Wessel <ejwessel@gmail.com>
2020-02-18 23:54:16 -0500

    Problem 113 Setup

---
+++ 113_PathSum2/Solution.py
@@ -0,0 +1,17 @@
+from typing import List
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def pathSum(self, root: TreeNode, sum: int) -> List[List[int]]:
+        pass
+
+if __name__ == "__main__":
+    sol = Solution()
+
+    sol.pathSum()
