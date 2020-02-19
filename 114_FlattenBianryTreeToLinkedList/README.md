7255a60536ef42b2139201e99e184d03ed4b5d95
Ethan Wessel <ethan.wessel@fluidity.io>
2020-02-19 15:00:47 -0500

    Problem 114 Analysis and second solution

--- 114_FlattenBianryTreeToLinkedList/README.md
+++ 114_FlattenBianryTreeToLinkedList/README.md
@@ -5,3 +5,6 @@ Runtime: O(n)
 
 Space: O(n)
 - tree is not balanced, entirety of tree could exist in the recursive stack
+
+The iterative solution is very simple, but seeing the pattern amongst the stack was not intuitive to me.
+I needed to understand that no 'None' nodes are added to the stack and that the next element in the stack is what the element should link to
--- 114_FlattenBianryTreeToLinkedList/Solution.py
+++ 114_FlattenBianryTreeToLinkedList/Solution.py
@@ -34,6 +34,32 @@ class Solution:
         # in the event there is no left or right, return just the node
         return root
 
+class Solution2:
+    def flatten(self, root: TreeNode) -> None:
+        if root is None:
+            return None
+
+        stack = [root]
+
+        while len(stack):
+            current = stack.pop()
+            # will not append 'None' nodes
+            if current.right:
+                stack.append(current.right)
+            if current.left:
+                stack.append(current.left)
+
+            current.left = None
+
+            # this is the trick:
+            # this works because we add right then left
+            # right is the next element we want as the right
+            # because the stack will visit the right element after the left tree, we can access it and link it
+            if len(stack) > 0:
+                current.right = stack[-1]
+            else:
+                current.right = None
+
 def printInOrder(node: TreeNode):
     if node is None:
         return []
@@ -143,4 +169,101 @@ if __name__ == "__main__":
     sol.flatten(node_1)
     output = printInOrder(node_1)
     assert output == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+    sol = Solution2()
+
+    node_1 = TreeNode(1)
+    node_2 = TreeNode(2)
+    node_3 = TreeNode(3)
+    node_4 = TreeNode(4)
+    node_1.left = node_2
+    node_2.left = node_3
+    node_3.left = node_4
+    output = printInOrder(node_1)
+    sol.flatten(node_1)
+    output = printInOrder(node_1)
+    assert output == [1, 2, 3, 4]
+
+
+    node_1 = TreeNode(1)
+    node_2 = TreeNode(2)
+    node_3 = TreeNode(3)
+    node_4 = TreeNode(4)
+    node_5 = TreeNode(5)
+    node_6 = TreeNode(6)
+    node_1.left = node_2
+    node_1.right = node_5
+    node_2.left = node_3
+    node_2.right = node_4
+    node_5.right = node_6
+    sol.flatten(node_1)
+    output = printInOrder(node_1)
+    assert output == [1, 2, 3, 4, 5, 6]
+
+    node_1 = TreeNode(1)
+    node_2 = TreeNode(2)
+    node_3 = TreeNode(3)
+    node_4 = TreeNode(4)
+    node_5 = TreeNode(5)
+    node_6 = TreeNode(6)
+    node_7 = TreeNode(7)
+    node_8 = TreeNode(8)
+    node_9 = TreeNode(9)
+    node_10 = TreeNode(10)
+    node_11 = TreeNode(11)
+    node_12 = TreeNode(12)
+    node_1.left = node_2
+    node_1.right = node_7
+    node_2.left = node_3
+    node_2.right = node_6
+    node_3.left = node_4
+    node_3.right = node_5
+    node_7.left = node_8
+    node_7.right = node_11
+    node_8.left = node_9
+    node_8.right = node_10
+    node_11.right = node_12
+    sol.flatten(node_1)
+    output = printInOrder(node_1)
+    assert output == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+    sol.flatten(node_1)
+    output = printInOrder(node_1)
+    assert output == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+    node_1 = TreeNode(1)
+    sol.flatten(node_1)
+    output = printInOrder(node_1)
+    assert output == [1]
+
+    node_1 = TreeNode(1)
+    node_2 = TreeNode(2)
+    node_3 = TreeNode(3)
+    node_4 = TreeNode(4)
+    node_5 = TreeNode(5)
+    node_6 = TreeNode(6)
+    node_7 = TreeNode(7)
+    node_8 = TreeNode(8)
+    node_9 = TreeNode(9)
+    node_10 = TreeNode(10)
+    node_11 = TreeNode(11)
+    node_12 = TreeNode(12)
+    node_1.left = node_2
+    node_2.left = node_3
+    node_3.left = node_4
+    node_4.left = node_5
+    node_5.left = node_6
+    node_6.left = node_7
+    node_7.left = node_8
+    node_8.left = node_9
+    node_9.left = node_10
+    node_10.left = node_11
+    node_11.left = node_12
+    output = printInOrder(node_1)
+    sol.flatten(node_1)
+    output = printInOrder(node_1)
+    assert output == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+    output = printInOrder(None)
+    assert output == []
 
