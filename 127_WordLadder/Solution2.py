97bbc8684cd77e4c00a671f619e28a6bf725a223
Ethan Wessel <ethan.wessel@fluidity.io>
2020-02-21 21:20:16 -0500

    optimize solution 2

--- 127_WordLadder/Solution2.py
+++ 127_WordLadder/Solution2.py
@@ -13,17 +13,24 @@ class Solution:
 
         queue = deque()
         queue.append((beginWord, 1))
+        visited = {}
 
         while queue:
             current_word, depth = queue.popleft()
 
+            # if we reach a word we've seen then return the depth we found it at
             if current_word == endWord:
                 return depth
 
+            # mark word we're at as visited
+            visited[current_word] = depth
+
             for i in range(len(current_word)):
                 key = current_word[:i] + '*' + current_word[i + 1:]
-                for prospective_words in word_combo[key]:
-                    queue.append((prospective_words, depth + 1))
+                for prospective_word in word_combo[key]:
+                    # only add words to queue we haven't seen before
+                    if prospective_word not in visited:
+                        queue.append((prospective_word, depth + 1))
                 # after adding everything zero out the list so that we can ignore it
                 word_combo[key] = []
 
@@ -36,6 +43,12 @@ if __name__ == "__main__":
     # assert not result
     # result = sol.isDistanceOne('hot', 'hog')
     # assert result
+    sol = Solution()
+    begin = "a"
+    end = "c"
+    wordList = ['a', 'b', 'c']
+    result = sol.ladderLength(begin, end, wordList)
+    assert result == 2
 
     sol = Solution()
     begin = 'hit'
