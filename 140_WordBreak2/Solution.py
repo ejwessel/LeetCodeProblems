c1d43d80443b6b79f19e7e0645bfa252883277d9
Ethan Wessel <ethan.wessel@fluidity.io>
2020-02-25 10:58:48 -0500

    Problem 140 Solution checkpoint

---
+++ 140_WordBreak2/Solution.py
@@ -0,0 +1,71 @@
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def __init__(self):
+        self.memo = defaultdict(list)
+
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        self._wordBreakHelper(s, wordDict)
+        # turn the solutions into strings
+        output = []
+        if s in self.memo:
+            for sol in self.memo[s]:
+                string_representation = ' '.join(sol)
+                output.append(string_representation)
+        return output
+
+    def _wordBreakHelper(self, s: str, wordDict: List[str]):
+        for i in range(1, len(s) + 1):
+            left_sub = s[:i]
+            right_sub = s[i:]
+
+            # don't continue if the left sub it not a word
+            if left_sub not in wordDict:
+                continue
+
+            # if the right sub is the end of the string save it
+            if right_sub is '':
+                # needs to be added as a list
+                self.memo[s].append([left_sub])
+                continue
+
+            # perform analysis on the right substring if it doesn't exist
+            if right_sub not in self.memo:
+                self._wordBreakHelper(right_sub, wordDict)
+
+            # if the right word is still not available then no solutions to add
+            if right_sub not in self.memo:
+                continue
+
+            # combine the solutions with left
+            for strings in self.memo[right_sub]:
+                self.memo[s].append([left_sub] + strings)
+
+
+
+if __name__ == "__main__":
+
+    s = "catsanddog"
+    wordDict = ["cat", "cats", "and", "sand", "dog"]
+    sol = Solution()
+    result = sol.wordBreak(s, wordDict)
+    assert result == ['cat sand dog', 'cats and dog']
+
+    s = "pineapplepenapple"
+    wordDict = ["apple", "pen", "applepen", "pine", "pineapple"]
+    sol = Solution()
+    result = sol.wordBreak(s, wordDict)
+    assert result == ['pine apple pen apple', 'pine applepen apple', 'pineapple pen apple']
+
+    s = "catsandog"
+    wordDict = ["cats", "dog", "sand", "and", "cat"]
+    sol = Solution()
+    result = sol.wordBreak(s, wordDict)
+    assert result == []
+
+    # s = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    # wordDict = ["a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa"]
+    # sol = Solution()
+    # result = sol.wordBreak(s, wordDict)
+    # print(result)
