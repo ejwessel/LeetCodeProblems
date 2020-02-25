d2ea55f0e0451c6e697655a545a5e889c88deb71
Ethan Wessel <ethan.wessel@fluidity.io>
2020-02-25 13:23:07 -0500

    Problem 141 Optimized

--- 141_LinkedListCycle/Solution.py
+++ 141_LinkedListCycle/Solution.py
@@ -5,7 +5,7 @@ class ListNode:
         self.next = None
 
 class Solution:
-    def hasCycle(self, head: ListNode) -> bool:
+    def hasCycleSet(self, head: ListNode) -> bool:
         seen_set = set()
         current = head
         while current is not None:
@@ -15,7 +15,58 @@ class Solution:
             current = current.next
         return False
 
+    def hasCycle(self, head: ListNode) -> bool:
+        if head is None:
+            return False
+        single = head
+        double = head.next
+
+        while double is not None:
+            # iterate double once
+            double = double.next
+            # check if we've seen single
+            if double is single:
+                return True
+            # iterate double twice
+            if double is not None:
+                double = double.next
+
+            # check if we've seen single
+            if double is single:
+                return True
+
+            # move single forward
+            single = single.next
+
+        return False
+
 if __name__ == "__main__":
+
+    node_3 = ListNode(3)
+    node_2 = ListNode(2)
+    node_0 = ListNode(0)
+    node_n4 = ListNode(-4)
+    node_3.next = node_2
+    node_2.next = node_0
+    node_0.next = node_n4
+    node_n4.next = node_2
+    sol = Solution()
+    result = sol.hasCycleSet(node_3)
+    assert result
+
+    node_3 = ListNode(3)
+    node_2 = ListNode(2)
+    node_3.next = node_2
+    node_2.next = node_3
+    result = sol.hasCycleSet(node_3)
+    assert result
+
+    result = sol.hasCycleSet(None)
+    assert not result
+
+    node_2 = ListNode(2)
+    result = sol.hasCycleSet(node_2)
+    assert not result
 
     node_3 = ListNode(3)
     node_2 = ListNode(2)
