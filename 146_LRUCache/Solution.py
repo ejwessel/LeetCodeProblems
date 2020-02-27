8b1060c4ab9942b8060419d54882791592825648
Ethan Wessel <ejwessel@gmail.com>
2020-02-26 23:24:16 -0500

    more test cases

--- 146_LRUCache/Solution.py
+++ 146_LRUCache/Solution.py
@@ -240,6 +240,23 @@ if __name__ == "__main__":
     output = print_linkedlist(lru_cache.linked_list.head)
     assert output == [1, 2, 3, 4]
 
+    cache = LRUCache(2)
+    cache.put(1, 1)
+    cache.put(2, 2)
+    assert cache.get(1) == 1
+    output = print_linkedlist(cache.linked_list.head)
+    assert output == [1, 2]
+    cache.put(3, 3); # evicts key 2
+    output = print_linkedlist(cache.linked_list.head)
+    assert output == [3, 1]
+    assert cache.get(2) == -1
+    cache.put(4, 4)  # evicts key 1
+    output = print_linkedlist(cache.linked_list.head)
+    assert output == [4, 3]
+    assert cache.get(1) == -1
+    assert cache.get(3) == 3
+    assert cache.get(4) == 4
+
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
