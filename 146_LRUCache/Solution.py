4a3478474fead00514b138384eda45c0de01c92c
Ethan Wessel <ethan.wessel@fluidity.io>
2020-02-27 10:11:28 -0500

    size bug

--- 146_LRUCache/Solution.py
+++ 146_LRUCache/Solution.py
@@ -53,15 +53,6 @@ class LinkedList:
             return element_to_remove
 
 
-def print_linkedlist(head: Node):
-    output = []
-    current = head
-    while current:
-        output.append(current.val)
-        current = current.next
-    return output
-
-
 class Bidict:
     def __init__(self):
         self.key_to_val = {}
@@ -106,6 +97,7 @@ class LRUCache:
             self.linked_list.tail.next = None
             node_to_move.prev = None
             self.linked_list.insertHead(node_to_move)
+            self.linked_list.size -= 1
             return node_to_move.val
         else:
             # link prev and next together
@@ -116,6 +108,7 @@ class LRUCache:
             node_to_move.prev = None
             # insert it into the beginning
             self.linked_list.insertHead(node_to_move)
+            self.linked_list.size -= 1
             return node_to_move.val
 
     def put(self, key: int, value: int) -> None:
@@ -142,6 +135,7 @@ class LRUCache:
                 self.linked_list.tail.next = None
                 node_to_move.prev = None
                 self.linked_list.insertHead(node_to_move)
+                self.linked_list.size -= 1
             # if the node is anywhere else
             else:
                 # link prev and next together
@@ -152,6 +146,7 @@ class LRUCache:
                 node_to_move.prev = None
                 # insert it into the beginning
                 self.linked_list.insertHead(node_to_move)
+                self.linked_list.size -= 1
         # if the the key value pair is new
         else:
             # insert
@@ -164,6 +159,15 @@ class LRUCache:
                 removed_node = self.linked_list.removeTail()
                 key_to_remove = self.nodes.val_to_key[removed_node]
                 self.nodes.remove_key(key_to_remove)
+
+
+def print_linkedlist(head: Node):
+    output = []
+    current = head
+    while current:
+        output.append(current.val)
+        current = current.next
+    return output
 
 
 def linkedlists_tests():
@@ -280,6 +284,41 @@ def lrucache_tests():
     assert cache.get(1) == -1
     assert cache.get(3) == 3
     assert cache.get(4) == 4
+
+    cache = LRUCache(10)
+    commands = ["put", "put", "put", "put", "put", "get", "put", "get", "get", "put", "get", "put", "put", "put",
+     "get", "put", "get", "get", "get", "get", "put", "put", "get", "get"]
+
+    # "get", "put", "put", "get", "put", "get",
+    #  "put", "get", "get", "get", "put", "put", "put", "get", "put", "get", "get", "put", "put", "get", "put", "put",
+    #  "put", "put", "get", "put", "put", "get", "put", "put", "get", "put", "put", "put", "put", "put", "get", "put",
+    #  "put", "get", "put", "get", "get", "get", "put", "get", "get", "put", "put", "put", "put", "get", "put", "put",
+    #  "put", "put", "get", "get", "get", "put", "put", "put", "get", "put", "put", "put", "get", "put", "put", "put",
+    #  "get", "get", "get", "put", "put", "put", "put", "get", "put", "put", "put", "put", "put", "put", "put"]
+    data = [[10, 13], [3, 17], [6, 11], [10, 5], [9, 10], [13], [2, 19], [2], [3], [5, 25], [8], [9, 22], [5, 5],
+     [1, 30], [11], [9, 12], [7], [5], [8], [9], [4, 30], [9, 3], [9], [10]]
+
+     # [10], [6, 14], [3, 1], [3], [10, 11], [8],
+     # [2, 14], [1], [5], [4], [11, 4], [12, 24], [5, 18], [13], [7, 23], [8], [12], [3, 27], [2, 12], [5], [2, 9],
+     # [13, 4], [8, 18], [1, 7], [6], [9, 29], [8, 21], [5], [6, 30], [1, 12], [10], [4, 15], [7, 22], [11, 26], [8, 17],
+     # [9, 29], [5], [3, 4], [11, 30], [12], [4, 29], [3], [9], [6], [3, 4], [1], [10], [3, 29], [10, 28], [1, 20],
+     # [11, 13], [3], [3, 12], [3, 8], [10, 9], [3, 26], [8], [7], [5], [13, 17], [2, 27], [11, 15], [12], [9, 19],
+     # [2, 15], [3, 16], [1], [12, 17], [9, 1], [6, 19], [4], [5], [5], [8, 1], [11, 7], [5, 2], [9, 28], [1], [2, 2],
+     # [7, 4], [4, 22], [7, 24], [9, 26], [13, 28], [11, 26]]
+
+    output = []
+    for command_i in range(len(commands)):
+        if commands[command_i] == "put":
+            key, value = data[command_i]
+            cache.put(key, value)
+            output.append(None)
+        else:
+            key = data[command_i][0]
+            value = cache.get(key)
+            output.append(value)
+
+    print(output)
+    assert output == [None,None,None,None,None,-1,None,19,17,None,-1,None,None,None,-1,None,-1,5,-1,12, None, None, 3, 5]
 
 if __name__ == "__main__":
     linkedlists_tests()
