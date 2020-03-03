919aee675e3568680e642414afda0b3126349fe3
Ethan Wessel <ethan.wessel@fluidity.io>
2020-03-03 16:41:10 -0500

    Problem 23 Priority Queue Solution

--- 23_MergeKSortedLists/Solution3.py
+++ 23_MergeKSortedLists/Solution3.py
@@ -1,5 +1,6 @@
 from typing import List
 
+
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, x):
@@ -11,21 +12,18 @@ class MinHeap:
     def __init__(self):
         self.capacity = 100
         self.heap = [None] * self.capacity
-        self.next_idx = 0
+        self.size = 0
 
     def get_parent(self, index):
-        if index % 2 == 0:
-            return (index - 2) // 2
-        else:
-            return (index - 1) // 2
+        return (index - 1) // 2
 
     def insert(self, new_node):
-        self.heap[self.next_idx] = new_node
+        self.heap[self.size] = new_node
         # heapify up
-        current_idx = self.next_idx
+        current_idx = self.size
         while current_idx != 0:
             parent = self.get_parent(current_idx)
-            if self.heap[current_idx] < self.heap[parent]:
+            if self.heap[current_idx].val < self.heap[parent].val:
                 temp = self.heap[parent]
                 self.heap[parent] = self.heap[current_idx]
                 self.heap[current_idx] = temp
@@ -33,24 +31,23 @@ class MinHeap:
             else:
                 break
 
-        # update the pointer to the next available spot
-        self.next_idx += 1
+        # increase size
+        self.size += 1
         # handle if we're at capacity
-        if self.next_idx + 1 == self.capacity:
+        if self.size + 1 == self.capacity:
             list_addendum = [None] * self.capacity
             self.heap += list_addendum
             self.capacity *= 2
 
     def getMin(self):
-        if self.next_idx <= 0:
+        if self.size <= 0:
             return None
 
         # handle getting element
         temp = self.heap[0]
-        self.heap[0] = self.heap[self.next_idx - 1]
-        self.heap[self.next_idx - 1] = temp
-        self.heap[self.next_idx - 1] = None
-        self.next_idx -= 1
+        self.heap[0] = self.heap[self.size - 1]
+        self.heap[self.size - 1] = None
+        self.size -= 1
         # element is removed and ready to be returned
         min_element = temp
 
@@ -61,20 +58,19 @@ class MinHeap:
             left_idx = current_idx * 2 + 1
             right_idx = current_idx * 2 + 2
 
-            if self.heap[left_idx] is None and self.heap[right_idx] is None:
-                idx_to_compare = None
-            elif self.heap[left_idx] is None:
-                idx_to_compare = right_idx
-            elif self.heap[right_idx] is None:
-                idx_to_compare = left_idx
-            else:
-                if self.heap[left_idx] < self.heap[right_idx]:
+            idx_to_compare = None
+            if left_idx < self.size and right_idx < self.size:
+                if self.heap[left_idx].val <= self.heap[right_idx].val:
                     idx_to_compare = left_idx
-                elif self.heap[right_idx] < self.heap[left_idx]:
+                elif self.heap[right_idx].val < self.heap[left_idx].val:
                     idx_to_compare = right_idx
+            elif left_idx < self.size:
+                idx_to_compare = left_idx
+            elif right_idx < self.size:
+                idx_to_compare = right_idx
 
             # do the comparision to determine if heapify
-            if idx_to_compare is not None and self.heap[idx_to_compare] < self.heap[current_idx]:
+            if idx_to_compare is not None and self.heap[idx_to_compare].val < self.heap[current_idx].val:
                 temp = self.heap[idx_to_compare]
                 self.heap[idx_to_compare] = self.heap[current_idx]
                 self.heap[current_idx] = temp
@@ -85,10 +81,32 @@ class MinHeap:
         return min_element
 
 
-
 class Solution:
     def mergeKLists(self, lists: List[ListNode]) -> ListNode:
-        pass
+        heap = MinHeap()
+        # add the linked lists to the heap
+        for list in lists:
+            if list is None:
+                continue
+            heap.insert(list)
+
+        head = None
+        tail = None
+        while heap.size > 0:
+            next_element = heap.getMin()
+            remaining_head = next_element.next
+            next_element.next = None
+            if remaining_head is not None:
+                heap.insert(remaining_head)
+            if head is None:
+                head = next_element
+                tail = head
+            else:
+                tail.next = next_element
+                tail = tail.next
+
+        return head
+
 
 def print_linked_list(head: ListNode):
     output = []
@@ -99,40 +117,117 @@ def print_linked_list(head: ListNode):
     return output
 
 
-if __name__ == "__main__":
-    minHeap = MinHeap()
-    minHeap.insert(1)
-    minHeap.insert(9)
-    minHeap.insert(6)
-    minHeap.insert(4)
-    minHeap.insert(0)
-    minHeap.insert(-1)
-    minHeap.insert(-1)
-    print(minHeap.heap)
+def create_linked_list(list):
+    head = None
+    tail = None
+    for item in list:
+        if head is None:
+            head = ListNode(item)
+            tail = head
+        else:
+            tail.next = ListNode(item)
+            tail = tail.next
+    return head
 
-    minHeap = MinHeap()
-    minHeap.insert(1)
-    minHeap.insert(9)
-    minHeap.insert(6)
-    minHeap.insert(4)
-    minHeap.insert(0)
-    minHeap.insert(-1)
-    minHeap.insert(-1)
-    result = minHeap.getMin()
-    print(result)
-    result = minHeap.getMin()
-    print(result)
-    result = minHeap.getMin()
-    print(result)
-    result = minHeap.getMin()
-    print(result)
-    result = minHeap.getMin()
-    print(result)
-    result = minHeap.getMin()
-    print(result)
-    result = minHeap.getMin()
-    print(result)
-    result = minHeap.getMin()
-    print(result)
-    result = minHeap.getMin()
-    print(result)
+
+if __name__ == "__main__":
+    # minHeap = MinHeap()
+    # minHeap.insert(1)
+    # minHeap.insert(9)
+    # minHeap.insert(6)
+    # minHeap.insert(4)
+    # minHeap.insert(0)
+    # minHeap.insert(-1)
+    # minHeap.insert(-1)
+    # print(minHeap.heap)
+    #
+    # minHeap = MinHeap()
+    # minHeap.insert(1)
+    # minHeap.insert(9)
+    # minHeap.insert(6)
+    # minHeap.insert(4)
+    # minHeap.insert(0)
+    # minHeap.insert(-1)
+    # minHeap.insert(-1)
+    # result = minHeap.getMin()
+    # print(result)
+    # result = minHeap.getMin()
+    # print(result)
+    # result = minHeap.getMin()
+    # print(result)
+    # result = minHeap.getMin()
+    # print(result)
+    # result = minHeap.getMin()
+    # print(result)
+    # result = minHeap.getMin()
+    # print(result)
+    # result = minHeap.getMin()
+    # print(result)
+    # result = minHeap.getMin()
+    # print(result)
+    # result = minHeap.getMin()
+    # print(result)
+
+    list_of_lists = [[-8, -7, -6, -3, -2, -2, 0, 3], [-10, -6, -4, -4, -4, -2, -1, 4], [-10, -9, -8, -8, -6],
+                     [-10, 0, 4]]
+    linked_lists = []
+    for list in list_of_lists:
+        new_ll = create_linked_list(list)
+        linked_lists.append(new_ll)
+    sol = Solution()
+    result = sol.mergeKLists(linked_lists)
+    output = print_linked_list(result)
+    assert output == [-10, -10, -10, -9, -8, -8, -8, -7, -6, -6, -6, -4, -4, -4, -3, -2, -2, -2, -1, 0, 0, 3, 4, 4]
+    print(output)
+
+    node1_1 = ListNode(1)
+    node1_2 = ListNode(4)
+    node1_3 = ListNode(5)
+    node1_1.next = node1_2
+    node1_2.next = node1_3
+    output = print_linked_list(node1_1)
+    assert output == [1, 4, 5]
+
+    node2_1 = ListNode(1)
+    node2_2 = ListNode(3)
+    node2_3 = ListNode(4)
+    node2_1.next = node2_2
+    node2_2.next = node2_3
+    output = print_linked_list(node2_1)
+    assert output == [1, 3, 4]
+
+    node3_1 = ListNode(2)
+    node3_2 = ListNode(6)
+    node3_1.next = node3_2
+    output = print_linked_list(node3_1)
+    assert output == [2, 6]
+
+    linked_lists = [node1_1, node2_1, node3_1]
+    sol = Solution()
+    result = sol.mergeKLists(linked_lists)
+    output = print_linked_list(result)
+    assert output == [1, 1, 2, 3, 4, 4, 5, 6]
+
+    node1_2 = ListNode(2)
+    node1_3 = ListNode(5)
+    linked_lists = [node1_2, None, node1_3]
+    result = sol.mergeKLists(linked_lists)
+    output = print_linked_list(result)
+    assert output == [2, 5]
+
+    node1_3 = ListNode(5)
+    linked_lists = [None, node1_3]
+    result = sol.mergeKLists(linked_lists)
+    output = print_linked_list(result)
+    assert output == [5]
+
+    linked_lists = [None, None]
+    result = sol.mergeKLists(linked_lists)
+    output = print_linked_list(result)
+    assert output == []
+
+    linked_lists = [None]
+    result = sol.mergeKLists(linked_lists)
+    output = print_linked_list(result)
+    assert output == []
+
