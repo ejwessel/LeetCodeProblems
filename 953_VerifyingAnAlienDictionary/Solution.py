85686af1e9f621256b4f6fb72fae65dfa215ca24
Ethan Wessel <ethan.wessel@fluidity.io>
2020-03-09 12:16:35 -0400

    Problem 953 Solution

--- 953_VerifyingAnAlienDictionary/Solution.py
+++ 953_VerifyingAnAlienDictionary/Solution.py
@@ -13,30 +13,35 @@ class Solution:
                 prev_word = words[w - 1]
                 current_word = words[w]
 
-                # compare the prefix first
-                prev_prefix = prev_word[:i]
-                current_prefix = current_word[:i]
+                # In the event there is no prefix compare the characters
+                if i == 0:
+                    prev_char = prev_word[i]
+                    current_char = current_word[i]
+                    if char_to_idx[prev_char] > char_to_idx[current_char]:
+                        return False
+                else:
+                    # compare the prefix first
+                    prev_prefix = prev_word[:i]
+                    current_prefix = current_word[:i]
 
-                if prev_prefix is '' and current_prefix is '':
-                    # no substring is empty, continue
-                    continue
-                elif prev_prefix != current_prefix:
-                    # prev prefix and current prefix are not comparable continue
-                    continue
-                # if index can't be reached then order is maintained
-                if i >= len(prev_word):
-                    continue
+                    if prev_prefix != current_prefix:
+                        # prev prefix and current prefix are not comparable continue
+                        continue
 
-                # if prev word is greater than current word order is not maintained
-                # don't compare words if the length is not at least 2
-                if i > 1 and len(prev_word) > len(current_word):
-                    return False
+                    # prefixes at this point are the same
 
-                # detect if ordering is incorrect
-                prev_char = prev_word[i]
-                current_char = current_word[i]
-                if char_to_idx[prev_char] > char_to_idx[current_char]:
-                    return False
+                    # if index can't be reached on prev word then order is maintained
+                    if i >= len(prev_word):
+                        continue
+                    # if index can't be reached on current word then order is maintained
+                    if i >= len(current_word):
+                        return False
+
+                    # detect if ordering is incorrect
+                    prev_char = prev_word[i]
+                    current_char = current_word[i]
+                    if char_to_idx[prev_char] > char_to_idx[current_char]:
+                        return False
 
         return True
 
@@ -61,11 +66,17 @@ if __name__ == "__main__":
                       'm': 12, 'n': 13, 'o': 14, 'p': 15, 'q': 16, 'r': 17, 's': 18, 't': 19, 'u': 20, 'v': 21, 'w': 22,
                       'x': 23, 'y': 24, 'z': 25}
 
+    output = sol.lenOfLongestWord(["hello", "leetcode"])
+    assert output == 8
+
     result = sol.isAlienSorted(["hall", "hallow", "heb", "hef"], "hlabcdefgijkmnopqrstuvwxyz")
     assert result
 
-    output = sol.lenOfLongestWord(["hello", "leetcode"])
-    assert output == 8
+    result = sol.isAlienSorted(["app", "apple", "ball"], "abcdefghijklmnopqrstuvwxyz")
+    assert result
+
+    result = sol.isAlienSorted(["apple", "app"], "abcdefghijklmnopqrstuvwxyz")
+    assert not result
 
     result = sol.isAlienSorted(["hello", "leetcode"], "hlabcdefgijkmnopqrstuvwxyz")
     assert result
@@ -73,9 +84,9 @@ if __name__ == "__main__":
     result = sol.isAlienSorted(["word", "world", "row"], "worldabcefghijkmnpqstuvxyz")
     assert not result
 
-    result = sol.isAlienSorted(["apple", "app"], "abcdefghijklmnopqrstuvwxyz")
-    assert not result
-
-    result = sol.isAlienSorted(["hall", "hallow", "halloween", "lab", "labrador", "apple", "barn"], "hlabcdefgijkmnopqrstuvwxyz")
+    result = sol.isAlienSorted(["hall", "hallow", "halloween", "lab", "labrador", "apple", "barn"],
+                               "hlabcdefgijkmnopqrstuvwxyz")
     assert result
 
+    result = sol.isAlienSorted(["my", "f"], "gelyriwxzdupkjctbfnqmsavho")
+    assert not result
